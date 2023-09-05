eeccd373f1b5c967d310c3a109d8b7d5ce859d67
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-05 18:42:06 +0300

    add mask for the number

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 import { useFormik } from "formik";
-
 import { isEmptyObj } from "minoru";
+import { maskJs } from "mask-js";
 
 import UiFormInput from "../UI/UiFormInput";
 import UiFormButton from "../UI/UiFormButton";
@@ -22,18 +22,20 @@ export const Form = ({ setFormBody }: Props) => {
       email: Yup.string()
         .email("*некорректный email")
         .required("*поле обязательно"),
-      password: Yup.string().min(6, "*слишком короткий пороль"),
+      number: Yup.string()
+        .min(6, "*слишком короткий номер")
+        .max(9, "*слишком длинный номер"),
     }),
     onSubmit: ({ email, number }) => {
       if (number) {
-        const formatNumber = String(number);
-        setFormBody({ email, number: formatNumber });
+        const validNum = number.replace(/-/g, "").slice(0, 6);
+        setFormBody({ email, number: validNum });
       } else {
         setFormBody({ email });
       }
     },
   });
-
+  // 822286
   return (
     <form onSubmit={formik.handleSubmit} className={styles.form}>
       <h1 className={styles.form__title}>Форма Поиска</h1>
@@ -49,11 +51,11 @@ export const Form = ({ setFormBody }: Props) => {
       />
       <UiFormInput
         name="number"
-        type="number"
+        type="string"
         title="Номер"
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
-        value={formik.values.number}
+        value={maskJs("99-99-99", formik.values.number)}
         errors={formik.errors.number}
         touched={formik.touched.number}
       />
